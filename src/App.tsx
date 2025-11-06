3918b053b8236f6524ee4a90d3ab076d7e6ef415
Taha Mlaiki <mlaikitaha29@gmail.com>
2025-11-06 15:52:03 +0100

    feat: react-router with guest & admin layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GuestLayout from "./pages/GuestLayout";
+import AdminLayout from "./pages/AdminLayout";
+import Home from "./pages/Home";
+import Simulation from "./pages/Simulation";
+import ApplicationForm from "./pages/ApplicationForm";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminDetail from "./pages/AdminDetail";
+
 function App() {
   return (
-    <>
-      <p className="bg-blue-500">taha</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<GuestLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/simulation" element={<Simulation />} />
+          <Route
+            path="/application/:simulationId"
+            element={<ApplicationForm />}
+          />
+        </Route>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/:id" element={<AdminDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
